01b91611f92d5eacc9ebce545f6c3dca06e588a8
Smaran Shaha <smaranshaha@gmail.com>
2024-12-22 22:12:02 +0600

    here i create listing page here user see the product and categori to select item i also create in backend product controlar for this

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
+import shopProductSlice from "./shop/products-slice/index";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
+    shopProducts: shopProductSlice,
   },
 });
 
